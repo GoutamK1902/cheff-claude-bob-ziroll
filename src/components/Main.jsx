c5769937959218e882cf4cdf83457fbc34f6d4e8
GoutamK1902 <goutamkhowal321@gmail.com>
2025-01-05 20:17:19 +0530

    Moved recipe and ingredients in separate components

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,12 +4,28 @@ import Ingredients from "./Ingredients";
 
 export default function Main(props) {
   // console.log(JSON.stringify(props));
-  const [ingredientsList, setIngredientsList] = useState([]);
-  console.log(ingredientsList);
+  const [ingredientsList, setIngredientsList] = useState([
+    "All the main indian spices",
+    "Paneer",
+    "Tamatar",
+    "Dhaniya",
+  ]);
+  // RECIPE COMPONENT
+  const [recipeShown, setRecipeShown] = useState(false);
+
+  function toggleRecipeShown() {
+    setRecipeShown((prev) => !prev);
+  }
+
+  // console.log(ingredientsList);
   return (
     <>
       <Form updatedList={setIngredientsList} />
-      <Ingredients ingredientsList={ingredientsList} />
+      <Ingredients
+        ingredientsList={ingredientsList}
+        toggleRecipeShown={toggleRecipeShown}
+        toShowRecipe={recipeShown}
+      />
     </>
   );
 }
